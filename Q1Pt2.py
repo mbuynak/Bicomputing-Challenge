b9c01f1c3108cc441083012efd60878a4f06c223
Michelle Corley <mcorley1@nd.edu>
2017-12-01 11:03:55 -0500

    linear regression and beginning of calculating nll for MLL

---
+++ Q1Pt2.py
@@ -0,0 +1,44 @@
+import numpy
+import pandas
+import scipy
+import scipy.integrate as spint
+from scipy.stats import norm
+from scipy.optimize import minimize
+from scipy.stats import chi2
+import plotnine
+from plotnine import *
+
+#Load Data
+data=pandas.read_csv("sugar.csv")
+
+#Generate plot that shows a linear regression of the E.coli growth with linear trendline
+plot1= ggplot(data,aes(x="sugar",y="growth"))+theme_classic()+geom_point()+stat_smooth(method="lm")
+print plot1
+
+
+dataFrame=pandas.DataFrame({'y':data.growth,'x':0})
+
+#Define null hypothesis 
+def nllikeNull(p,obs):
+    B0=p[0]
+    sigma=p[1]
+    
+    expected=B0
+    nll=-1*norm(expected,sigma).logpdf(obs.y).sum()
+    return nll
+    
+# estimate parameters by minimizing the NLL for data
+initialGuess=numpy.array([1,1])
+fitNull=minimize(nllikeNull,initialGuess,method="Nelder-Mead",options={'disp': True},args=dataFrame)
+nllNull= fit.fun #gives NLL value for null
+print(nllNull)
+
+#Define Alternative
+def nllike(p,obs):
+    B0=p[0]
+    B1=p[1]
+    sigma=p[2]
+    
+    expectedAlt=B0+B1*obs.x
+    nll=-1*norm(expectedAlt,sigma).logpdf(obs.y).sum()
+    return nll
